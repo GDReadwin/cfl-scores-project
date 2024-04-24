245cefbb548bef149deec4b9bea0ae84f1fdd295
GDReadwin <gerald.readwin@edu.sait.ca>
2024-04-23 18:29:42 -0600

    colour update

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import Image from "next/image";
 const teams = [
  { name: "British Colombia Lions", primaryColor: "#f15524", secondaryColor: "#000000", tertiaryColor: "#ffffff", clicked: false, imagepath: './public/images/image1.jpg', greyCupWins: 6, mostRecentWin: "2011", lastSeasonRecord: "12-6-0-24" },
  { name: "Calgary Stampeders", primaryColor: "#d0202d", secondaryColor: "#ffffff", tertiaryColor: "#000000", clicked: false, imagepath: './public/images/image2.jpg', greyCupWins: 8, mostRecentWin: "2018", lastSeasonRecord: "6-12-0-12" },
- { name: "Edmonton Elk", primaryColor: "#ffb612", secondaryColor: "#2b5235", tertiaryColor: "#ffffff", clicked: false, imagepath: './public/images/image3.jpg', greyCupWins: 14, mostRecentWin: "2015", lastSeasonRecord: "4-14-0-8" },
+ { name: "Edmonton Elk", primaryColor: "#2b5235", secondaryColor: "#ffb612", tertiaryColor: "#ffffff", clicked: false, imagepath: './public/images/image3.jpg', greyCupWins: 14, mostRecentWin: "2015", lastSeasonRecord: "4-14-0-8" },
  { name: "Hamilton Tiger-Cats", primaryColor: "#ffb612", secondaryColor: "#ffffff", tertiaryColor: "#000000", clicked: false, imagepath: './public/images/image4.jpg', greyCupWins: 8, mostRecentWin: "1999", lastSeasonRecord: "8-10-0-16" },
  { name: "Montreal Alouettes", primaryColor: "#183c6e", secondaryColor: "#8c2334", tertiaryColor: "#a2a5aa", clicked: false, imagepath: './public/images/image5.jpg', greyCupWins: 8, mostRecentWin: "2023", lastSeasonRecord: "11-7-0-22" },
  { name: "Ottawa Redblacks", primaryColor: "#000000", secondaryColor: "#ffffff", tertiaryColor: "#a71930", clicked: false, imagepath: './public/images/image6.jpg', greyCupWins: 1, mostRecentWin: "2016", lastSeasonRecord: "4-14-0-8" },
@@ -60,6 +60,21 @@ export default function Home() {
           <p>Number of Grey Cup Wins: {selectedTeam.greyCupWins}</p>
           <p>Most Recent Grey Cup Win: {selectedTeam.mostRecentWin}</p>
           <p>Last Season Record: {selectedTeam.lastSeasonRecord}</p>
+          {/* New section for displaying team colors */}
+          <div className="flex justify-center mt-4">
+            <div className="flex flex-col items-center">
+              <div style={{ backgroundColor: selectedTeam.primaryColor, width: '50px', height: '50px' }}></div>
+              <p>Primary Color</p>
+            </div>
+            <div className="flex flex-col items-center ml-4">
+              <div style={{ backgroundColor: selectedTeam.secondaryColor, width: '50px', height: '50px' }}></div>
+              <p>Secondary Color</p>
+            </div>
+            <div className="flex flex-col items-center ml-4">
+              <div style={{ backgroundColor: selectedTeam.tertiaryColor, width: '50px', height: '50px' }}></div>
+              <p>Tertiary Color</p>
+            </div>
+          </div>
         </div>
       )}
    
